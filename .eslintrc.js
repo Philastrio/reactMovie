a98d5398877fcbb2577555e3c6531bb0784b3758
KANG <rio790@empas.com>
2019-02-09 15:26:30 +0900

    ajax

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,3 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-        "linebreak-style": "off",
-        "indent": "off",
-        "no-tabs": "off",
-        "no-mixed-spaces-and-tabs": "off",
-        "comma-dangle": "off",
-        "no-trailing-spaces": "off",
-        "no-unused-vars": "off"
-    },
-    'env': {
-        'browser': true
-    }
+    "extends": "airbnb"
 };
--- src/App.js
+++ src/App.js
@@ -5,35 +5,11 @@ import Movie from './Movie';
 class App extends Component {
   state = {
 
-  };
+  }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-        {
-          title: 'Matrix',
-          poster: "https://prodeoetpatria.files.wordpress.com/2011/04/the-matrix.jpg",
-        },
-        {
-          title: 'Full Metal Jacket',
-          poster: "https://thesouloftheplot.files.wordpress.com/2014/06/poster_fullmetaljacket.jpg",
-        },
-        {
-          title: 'Oldboy',
-          poster: "http://nickyarborough.com/wp-content/uploads/2015/06/oldboy.jpg",
-        },
-        {
-          title: 'Star Wars',
-          poster: "http://2.bp.blogspot.com/_fM0KTi1Y_UU/TFRq8_i5I2I/AAAAAAAAAQ0/nokTrdM_934/s1600/star+wars+wallpaper+5.jpg"
-        },
-        {
-          title: "바람과 함께 놀러가다",
-          poster: 'http://ojsfile.ohmynews.com/STD_IMG_FILE/2015/0216/IE001801808_STD.jpg'
-        },
-        ]
-      })
-    }, 5000)
+    fetch('https://yts.lu/api/v2/list_movies.json?sort_by=rating')
+    console.log('hello');
   }
 
   _renderMovies = () => {
