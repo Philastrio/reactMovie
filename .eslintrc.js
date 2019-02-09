59eb9eabd484457273f8c1fd4251c57c2dab67b0
philastrio <han_gang@naer.com>
2019-02-10 07:46:19 +0900

    처음만드는 React Movie App 완성!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb-base",
+    "rules": {
+        "linebreak-style": "off",
+        "indent": "off",
+        "no-tabs": "off",
+        "no-mixed-spaces-and-tabs": "off",
+        "comma-dangle": "off",
+        "no-trailing-spaces": "off",
+        "no-unused-vars": "off"
+    },
+    'env': {
+        'browser': true
+    }
 };
